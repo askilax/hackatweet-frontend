fa4ba5f5d96338b3211fb1750e314d777c7bf39e
Maxime Sbaizero <maximesbaizero@gmail.com>
2024-10-09 18:31:40 +0200

    mep

---
+++ pages/_app.js
@@ -0,0 +1,37 @@
+import '../styles/globals.css';
+import Head from 'next/head';
+
+//redux-persist
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+
+const reducers = combineReducers({ nom_des_reducer });
+const persistConfig = { key: 'Hackatweet', storage };
+
+const store = configureStore({
+  reducer: persistReducer(persistConfig, reducers),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+});
+
+const persistor = persistStore(store);
+
+function App({ Component, pageProps }) {
+  return (
+    <>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <Head>
+            <title>Next.js App</title>
+          </Head>
+          <Component {...pageProps} />
+        </PersistGate>
+      </Provider>
+    </>
+  );
+}
+
+export default App;
