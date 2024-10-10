6e6ed4b3f74a9a22bad87eb9b558b58bd232fa4d
Maxime Sbaizero <maximesbaizero@gmail.com>
2024-10-10 15:27:52 +0200

    mise a jour home

--- components/Home.js
+++ components/Home.js
@@ -4,7 +4,23 @@ function Home() {
   return (
     <div><main className={styles.main}>
       <div className={styles.containerHome}>
-        containerHome
+        <div className={styles.containerMenu}>
+        <img src='./logotweeter.png' className={styles.imgProfil}></img>
+          <ul>
+            <li>Accueil</li>
+            <li>Explorer</li>
+            <li>Notifications</li>
+            <li>Messages</li>
+            <li>Signets</li>
+            <li>Listes</li>
+            <li>Profil</li>
+            <li>Plus</li>
+            <button>Tweeter</button>
+          </ul>
+        </div>
+        <div className={styles.containerProfil}>
+          <img src='./profil.jpg' className={styles.imgProfil}></img>
+        </div>
       </div>
       <div className={styles.containerTweetAndLast}>
         <div className={styles.containerTweet}>
