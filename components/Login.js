63d2518b6aeaec9c436944958cc956393c015179
Your name <Your email>
2024-10-10 14:03:29 +0200

    commitimages

--- components/Login.js
+++ components/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styles from '../styles/Login.module.css';
 import SignUp from "./SignUp";
 import SignIn from "./SignIn";
+import Image from 'next/image';
 
 function Login() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -17,11 +18,12 @@ function Login() {
   };
 
   return (
-    <div className="login-container">
+    <div className={styles.logincontainer}>
+        <div><Image src={props.urlToImage} alt={props.title} width={600} height={314} /></div>
       <h1>See what's happening</h1>
       <p>Join Hackatweet today.</p>
-      <button onClick={() => handleOpenModal("signup")}>Sign up</button>
-      <button onClick={() => handleOpenModal("signin")}>Sign in</button>
+      <button onClick={() => handleOpenModal("signup")} className={styles.button}>Sign up</button>
+      <button onClick={() => handleOpenModal("signin")} className={styles.button}>Sign in</button>
 
       {isModalOpen && modalType === "signup" && <SignUp closeModal={handleCloseModal} />}
       {isModalOpen && modalType === "signin" && <SignIn closeModal={handleCloseModal} />}
