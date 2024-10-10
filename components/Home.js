7bf649730075e95b19776641ba596ff4f89dc882
Maxime Sbaizero <maximesbaizero@gmail.com>
2024-10-10 16:56:03 +0200

    mise a jour home

--- components/Home.js
+++ components/Home.js
@@ -1,21 +1,24 @@
 import styles from '../styles/Home.module.css';
+import 'antd/dist/antd.css'
+
+
 
 function Home() {
   return (
     <div><main className={styles.main}>
       <div className={styles.containerHome}>
         <div className={styles.containerMenu}>
-        <img src='./logotweeter.png' className={styles.imgProfil}></img>
-          <ul>
-            <li>Accueil</li>
-            <li>Explorer</li>
-            <li>Notifications</li>
-            <li>Messages</li>
-            <li>Signets</li>
-            <li>Listes</li>
-            <li>Profil</li>
-            <li>Plus</li>
-            <button>Tweeter</button>
+        <img src='./logotweeter.png' className={styles.imgLogo}></img>
+          <ul className={styles.menu}>
+            <li className={styles.menuLi}>Accueil</li>
+            <li className={styles.menuLi}>Explorer</li>
+            <li className={styles.menuLi}>Notifications</li>
+            <li className={styles.menuLi}>Messages</li>
+            <li className={styles.menuLi}>Signets</li>
+            <li className={styles.menuLi}>Listes</li>
+            <li className={styles.menuLi}>Profil</li>
+            <li className={styles.menuLi}>Plus</li>
+            <button className={styles.btn1}>Tweeter</button>
           </ul>
         </div>
         <div className={styles.containerProfil}>
