facc6e24f3b4f0018b8c8b60450dab8bfb9acc4e
Maxime Sbaizero <maximesbaizero@gmail.com>
2024-10-10 17:48:14 +0200

    correct

--- components/Tweet.js
+++ components/Tweet.js
@@ -3,14 +3,21 @@ import { useState } from 'react';
 
 function Tweets() {
 const [value,SetValue] = useState('')
-
+const maxLength = 280;
+const handleChange = ()=> {
+    if(e.target.value.length < maxLength){
+        SetValue(e.target.value);
+    }
+}
 
   return (
     <div>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          
+          Home
         </h1>
+        <input />
+        <span></span>
       </main>
     </div>
   );
