57aed190eb5d961ccdbf481e0430ab895242d992
Maxime Sbaizero <maximesbaizero@gmail.com>
2024-10-11 12:16:08 +0200

    fin de la page login

--- components/LastTweets.js
+++ components/LastTweets.js
@@ -1,20 +1,17 @@
 import styles from '../styles/LastTweets.module.css';
+import { useSelector } from 'react-redux';
 
 const LastTweets = ({ tweet }) => {
+    const firstName = useSelector((state) => state.user.firstName);
+    const userName = useSelector((state) => state.user.userName);
+
     return (
-        // <div className="tweet">
-        //   <div className="user-info">
-        //      <img src={tweet.user.avatar} alt="avatar" />
-        //     <span>{tweet.user.name} @{tweet.user.handle}</span>
-        //   </div>
-        //    <p>{tweet.content}</p> 
-        // </div>
         <div className={styles.tweet}>
-            <div className="user-info">
+        <div className={styles.userInfo}>
                 <img src='./profil.jpg' alt="avatar" className={styles.imgProfil} />
-                <span className={styles.name}>name</span>
-                <span className={styles.username}>@username</span>
-                <span className={styles.data}> . data</span>
+                <p className={styles.name}>{firstName || John}</p>
+                <p className={styles.username}>{userName || '@John'}</p>
+                <p className={styles.data}> . il y a 15 minute</p>
             </div>
             <p className={styles.p}>Le monde évolue rapidement avec la technologie. Chaque jour, de nouvelles innovations transforment notre façon de vivre, de communiquer et de travailler. Dans ce contexte, il est essentiel de rester informé et d'apprendre en continu pour s'adapter et tirer parti des opportunités que cela offre.</p>
         </div>
--- components/SignUp.js
+++ components/SignUp.js
@@ -1,45 +1,51 @@
 import React, { useState } from "react";
 import styles from '../styles/SignUp.module.css';
+import { useDispatch } from "react-redux";
+import { setUserData } from "../reducers/user";
+
 
 function SignUp({ closeModal }) {
-  const [formData, setFormData] = useState({ username: "", password: "" });
+  const dispatch = useDispatch()
+  const [formData, setFormData] = useState({ userName: "", password: "" });
+
 
   const handleInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    fetch("http://localhost:3000/users/signup", {
+    const response = await fetch("http://localhost:3000/users/signup", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
     })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        
-        if (data.token) {
-          localStorage.setItem("token", data.token);
-          window.location.href = "/Home";
-        }
-      })
-  };
+    const data = await response.json()
+    if (data.token) {
+      const userData = {
+        token: data.token,
+        firstName: data.firstName,
+        userName: data.userName,
+      };
+      dispatch(setUserData(userData))
+      window.location.href = "/Home";
+    }
+  }
 
   return (
     <div className="modal">
       <h2>Sign Up</h2>
       <form onSubmit={handleSubmit} className={styles.SignUp}>
         <input
-        type="text"
-        name="firstname"
-        placeholder="Firstname"
-        onChange={handleInputChange}
-        className={styles.input}
-      />
+          type="text"
+          name="firstName"
+          placeholder="Firstname"
+          onChange={handleInputChange}
+          className={styles.input}
+        />
         <input
           type="text"
-          name="username"
+          name="userName"
           placeholder="Username"
           onChange={handleInputChange}
           className={styles.input}
