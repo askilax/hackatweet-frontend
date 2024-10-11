182e170d3434ff9c6d09413f36e457d94de6e7c9
Your name <Your email>
2024-10-11 17:04:15 +0200

    commitcorbeille

--- components/LastTweets.js
+++ components/LastTweets.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import styles from '../styles/LastTweets.module.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import moment from 'moment';
 import 'moment/locale/fr';
 import { FaHeart, FaTrash } from 'react-icons/fa'; // Importer des icônes
 moment.locale('fr');
 
 const LastTweets = () => {
+    const dispatch = useDispatch(); // Utiliser pour déclencher les actions Redux
     const firstName = useSelector((state) => state.user.firstName);
     const userName = useSelector((state) => state.user.userName);
     const allTweets = useSelector((state) => state.tweets.value.allTweets || []);
@@ -14,11 +15,25 @@ const LastTweets = () => {
     // Trier les tweets par date (du plus récent au plus ancien)
     const sortedTweets = [...allTweets].sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
 
+    // Fonction pour supprimer un tweet
+    const handleDeleteTweet = (id) => {
+        dispatch({
+            type: 'tweets/deleteTweet',
+            payload: id
+        });
+    };
+
     return (
         <div className={styles.tweetsList}>
             {sortedTweets.length > 0 ? (
                 sortedTweets.map((tweet) => (
-                    <Tweet key={tweet.id} tweet={tweet} firstName={firstName} userName={userName} />
+                    <Tweet
+                        key={tweet.id}
+                        tweet={tweet}
+                        firstName={firstName}
+                        userName={userName}
+                        onDelete={() => handleDeleteTweet(tweet.id)} // Passer la fonction de suppression
+                    />
                 ))
             ) : (
                 <p className={styles.noTweets}>Aucun tweet pour le moment</p>
@@ -27,8 +42,8 @@ const LastTweets = () => {
     );
 };
 
-// Composant pour un tweet individuel avec gestion des likes
-const Tweet = ({ tweet, firstName, userName }) => {
+// Composant pour un tweet individuel avec gestion des likes et suppression
+const Tweet = ({ tweet, firstName, userName, onDelete }) => {
     const [likes, setLikes] = useState(0); // État pour gérer le nombre de likes
 
     const handleLikeClick = () => {
@@ -54,7 +69,7 @@ const Tweet = ({ tweet, firstName, userName }) => {
             <div className={styles.actions}>
                 <FaHeart className={styles.iconHeart} onClick={handleLikeClick} /> {/* Icône de cœur avec gestion du clic */}
                 <span>{likes}</span> {/* Affiche le nombre de likes */}
-                <FaTrash className={styles.iconTrash} /> {/* Icône de corbeille */}
+                <FaTrash className={styles.iconTrash} onClick={onDelete} /> {/* Icône de corbeille avec suppression */}
             </div>
         </div>
     );
